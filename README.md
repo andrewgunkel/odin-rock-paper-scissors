2b857fe5af71f5cc0ee078567fcc31d058954ee3
Andrew Gunkel <65040985+andrewgunkel@users.noreply.github.com>
2025-11-22 19:23:28 +0100

    FIX cleanup of consts and lets, structure changes for readability

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # odin-rock-paper-scissors
 Odin Project: Rock Paper Scissors
+
+View the brief: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors 
+
+Try it: https://andrewgunkel.github.io/odin-rock-paper-scissors/index.html 
--- script.js
+++ script.js
@@ -2,29 +2,23 @@ let getComputerChoice
 let humanScore = 0
 let computerScore = 0
 
-x = 1;
+let round = 1
 
-
-function humanChoice() {
-    
-return humanChoiceLowerCase
-}
-round = 1
+//Below function plays a single round
 
 function playRound() {
 
-    roundNum = round; 
     console.log ("Round " + round + " is starting now: ")
-    let getHumanChoice = prompt("Type rock, paper or scissors to play");
-    let humanChoiceLowerCase = (getHumanChoice.toLowerCase());
-    let randomNumber = Math.floor((Math.random() * 100 )+ 1);
+    const getHumanChoice = prompt("Type rock, paper or scissors to play");
+    const humanChoiceLowerCase = (getHumanChoice.toLowerCase());
+    const randomNumber = Math.floor((Math.random() * 100 )+ 1);
     
     if (randomNumber < 33) {
     getComputerChoice="rock"
     }  else if (randomNumber <66) {
     getComputerChoice="paper"
     } else {
-        getComputerChoice="scissors"
+    getComputerChoice="scissors"
     }
 
     console.log("You have picked " + humanChoiceLowerCase + "...")
@@ -34,22 +28,22 @@ function playRound() {
     console.log("...")
 if (getComputerChoice ===  humanChoiceLowerCase){
     console.log("Draw, play again!")
-    } else if (humanChoiceLowerCase=="rock" && getComputerChoice=="scissors"){
+    } else if (humanChoiceLowerCase==="rock" && getComputerChoice==="scissors"){
         console.log("Rock beats scissors! You win this round!")
         humanScore= humanScore + 1;
-    } else if (humanChoiceLowerCase=="rock" && getComputerChoice=="paper"){
+    } else if (humanChoiceLowerCase==="rock" && getComputerChoice==="paper"){
         computerScore= computerScore + 1;
         console.log("Unlucky! Paper beats rock. You lose this round.")
-    } else if (humanChoiceLowerCase=="scissors" && getComputerChoice=="paper") {
+    } else if (humanChoiceLowerCase==="scissors" && getComputerChoice==="paper") {
         console.log("Scissors beats paper! You win this round!")
         humanScore= humanScore + 1;
-    } else if (humanChoiceLowerCase=="paper" && getComputerChoice=="rock")  {
+    } else if (humanChoiceLowerCase==="paper" && getComputerChoice==="rock")  {
         console.log("Paper beats rock! You win this round!")
         humanScore= humanScore + 1;
-    } else if (humanChoiceLowerCase=="scissors" && getComputerChoice=="rock") {
+    } else if (humanChoiceLowerCase==="scissors" && getComputerChoice==="rock") {
         computerScore= computerScore + 1;
         console.log("Unlucky! Rock beats scissors. You lose this round.")
-    } else if (humanChoiceLowerCase=="scissors" && getComputerChoice=="paper") {
+    } else if (humanChoiceLowerCase==="paper" && getComputerChoice==="scissors") {
         computerScore= computerScore + 1;
         console.log("Unlucky! Scissors beats paper. You lose this round.")
     } else {
@@ -61,15 +55,13 @@ console.log("Round " + round + " of 5 is over.")
 round = ++round;
 }
 
+// Below for loop plays 5 rounds 
 
-for (let round = 1; round <6; round++) {
-let round 
+for (let i = 1; i < 6; i++) {
 playRound ();
-round = 1;
-round = ++round;
-
 }
 
+// Below if statement determines the winner
 
 if (humanScore > computerScore) {
     console.log("Game over! You win!")
