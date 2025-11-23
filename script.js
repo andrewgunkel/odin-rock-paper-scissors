3226e68d2adcf93286a0ade01af8ccf7a2e0dc7a
Andrew Gunkel <65040985+andrewgunkel@users.noreply.github.com>
2025-11-23 09:43:54 +0100

    FIX cleanup structure of js file

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let computerScore = 0
 
 let round = 1
 
-//Below function plays a single round
-
 function playRound() {
 
     console.log ("Round " + round + " is starting now: ")
@@ -14,11 +12,11 @@ function playRound() {
     const randomNumber = Math.floor((Math.random() * 100 )+ 1);
     
     if (randomNumber < 33) {
-    getComputerChoice="rock"
+        getComputerChoice="rock"
     }  else if (randomNumber <66) {
-    getComputerChoice="paper"
+        getComputerChoice="paper"
     } else {
-    getComputerChoice="scissors"
+        getComputerChoice="scissors"
     }
 
     console.log("You have picked " + humanChoiceLowerCase + "...")
@@ -26,42 +24,40 @@ function playRound() {
     console.log("And the ghost in the machine has picked " + getComputerChoice + "!")
     console.log("...")
     console.log("...")
-if (getComputerChoice ===  humanChoiceLowerCase){
-    console.log("Draw, play again!")
-    } else if (humanChoiceLowerCase==="rock" && getComputerChoice==="scissors"){
-        console.log("Rock beats scissors! You win this round!")
-        humanScore= humanScore + 1;
-    } else if (humanChoiceLowerCase==="rock" && getComputerChoice==="paper"){
-        computerScore= computerScore + 1;
-        console.log("Unlucky! Paper beats rock. You lose this round.")
-    } else if (humanChoiceLowerCase==="scissors" && getComputerChoice==="paper") {
-        console.log("Scissors beats paper! You win this round!")
-        humanScore= humanScore + 1;
-    } else if (humanChoiceLowerCase==="paper" && getComputerChoice==="rock")  {
-        console.log("Paper beats rock! You win this round!")
-        humanScore= humanScore + 1;
-    } else if (humanChoiceLowerCase==="scissors" && getComputerChoice==="rock") {
-        computerScore= computerScore + 1;
-        console.log("Unlucky! Rock beats scissors. You lose this round.")
-    } else if (humanChoiceLowerCase==="paper" && getComputerChoice==="scissors") {
-        computerScore= computerScore + 1;
-        console.log("Unlucky! Scissors beats paper. You lose this round.")
-    } else {
-        console.log("I didn't understand your choice, pick again!")
-    }
+    if (getComputerChoice ===  humanChoiceLowerCase){
+        console.log("Draw, play again!")
+        } else if (humanChoiceLowerCase==="rock" && getComputerChoice==="scissors"){
+            console.log("Rock beats scissors! You win this round!")
+            humanScore= humanScore + 1;
+        } else if (humanChoiceLowerCase==="rock" && getComputerChoice==="paper"){
+            computerScore= computerScore + 1;
+            console.log("Unlucky! Paper beats rock. You lose this round.")
+        } else if (humanChoiceLowerCase==="scissors" && getComputerChoice==="paper") {
+            console.log("Scissors beats paper! You win this round!")
+            humanScore= humanScore + 1;
+        } else if (humanChoiceLowerCase==="paper" && getComputerChoice==="rock")  {
+            console.log("Paper beats rock! You win this round!")
+            humanScore= humanScore + 1;
+        } else if (humanChoiceLowerCase==="scissors" && getComputerChoice==="rock") {
+            computerScore= computerScore + 1;
+            console.log("Unlucky! Rock beats scissors. You lose this round.")
+        } else if (humanChoiceLowerCase==="paper" && getComputerChoice==="scissors") {
+            computerScore= computerScore + 1;
+            console.log("Unlucky! Scissors beats paper. You lose this round.")
+        } else {
+            console.log("I didn't understand your choice, pick again!")
+        }
     console.log("Your score is " + humanScore)
-console.log("The ghost in the machine's score is " + computerScore) 
-console.log("Round " + round + " of 5 is over.")
-round = ++round;
+    console.log("The ghost in the machine's score is " + computerScore) 
+    console.log("Round " + round + " of 5 is over.")
+    round = ++round;
 }
 
 // Below for loop plays 5 rounds 
 
 for (let i = 1; i < 6; i++) {
-playRound ();
+    playRound ();
 }
-
-// Below if statement determines the winner
 
 if (humanScore > computerScore) {
     console.log("Game over! You win!")
